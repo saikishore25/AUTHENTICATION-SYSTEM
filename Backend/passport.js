ff2218034dce5c4dd4405a2c860074cba3be591b
Kishore <makam.kishore.991.sandhya@gmail.com>
2025-04-14 12:35:29 +0530

    commit

--- Backend/passport.js
+++ Backend/passport.js
@@ -12,7 +12,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: `https://authentication-system-frontend-taupe.vercel.app/api/auth/google/callback`,
+            callbackURL: `https://authentication-system-backend-sand.vercel.app/api/auth/google/callback`,
             scope: ["profile", "email"],
         },
         async (accessToken, refreshToken, profile, done) => {   
